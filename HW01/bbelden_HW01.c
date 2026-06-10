38ce71039b8aa030678962b35edb9918e04c4332
Ben Belden <bbelden@uab.edu>
2026-06-10 01:26:24 +0000

    UABMaxMinDiff function

--- HW01/bbelden_HW01.c
+++ HW01/bbelden_HW01.c
@@ -25,10 +25,34 @@ int sumOfDigits(int num) {
 
 }
 
+int UABMaxMinDiff(int arr[], int size) {
+
+    int min = arr[0];
+    int max = arr[0];
+
+    for (int i = 0; i < size; i++) {
+        if (arr[i] < min) {
+            min = arr[i];
+        } if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+
+    int diff = max - min;
+    return diff;
+
+}
+
 int main() {
 
-    int mySum = sumOfDigits(-308);
+    int mySum = sumOfDigits(123);
     printf("The sum of the digits represented from the integer passed in as a parameter by the user is: %d\n", mySum);
 
+    int arr[] = {-2, 4, -1, 6, 5};
+    int size_arr = sizeof(arr) / sizeof(arr[0]);
+    int myDiff = UABMaxMinDiff(arr, size_arr);
+    printf("From the array passed in by the user, the integer value computed by subtracting the maximum value from the minimum value from the array is: %d\n", myDiff);
+
     return 0;
+
 }
