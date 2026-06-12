98df2662f733d15dd7f43bed6da91922540789f9
Ben Belden <bbelden@uab.edu>
2026-06-12 21:50:37 +0000

    final submission, yo

--- HW01/bbelden_HW01.c
+++ HW01/bbelden_HW01.c
@@ -51,20 +51,56 @@ void replaceEvenWithZero(int *p, int size) {
         }
         p++;  
     }
+
 }
 
+bool perfectSquare(int n) {
+
+    for (int i = 0; i <= n / 2; i++) {
+        if (i * i == n) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+int countVowels(char *l, int size) {
+
+    int numOfVowels = 0;
+
+    char vowels[10] = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+
+    for (int i = 0; i < size; i++) {
+        for (int n = 0; n < 10; n++) {
+            if (*l == vowels[n]) {
+                numOfVowels += 1;
+            }
+        }
+        l++;
+    }
+
+    return numOfVowels;
+
+}
 
 
 int main() {
 
+    /* intro */ 
+    printf("\nblazerID: bbelden || Systems Programming\n");
+
+    /* call to 'sumOfDigits' */
     int mySum = sumOfDigits(123);
     printf("the sum of the digits represented from the integer passed in as a parameter by the user is: %d\n", mySum);
 
+    /* call to 'UABMaxMinDiff' */
     int arr[] = {-2, 4, -1, 6, 5};
     int size_arr = sizeof(arr) / sizeof(arr[0]);
     int myDiff = UABMaxMinDiff(arr, size_arr);
     printf("from the array passed in by the user, the integer value computed by subtracting the maximum value from the minimum value from the array is: %d\n", myDiff);
 
+    /* call to 'replaceEvenWithZero' */
     int arr2[] = {1, 3, 5};
     int size_arr2 = sizeof(arr2) / sizeof(arr2[0]);
     int *ptr1;
@@ -76,6 +112,26 @@ int main() {
         ptr1++;  
     }
     printf("\n");
+
+    /* call to 'perfectSquare' */
+    int numPerfect = 36;
+    bool isPerfectSquare = perfectSquare(numPerfect);
+    if (isPerfectSquare) {
+        printf("the number %d is a perfect square.\n", numPerfect);
+    } else {
+        printf("the number %d is not a perfect square.\n", numPerfect);
+    }
+
+    /* call to 'countVowels' */
+    char str[] = "Clayface";
+    int size_str = sizeof(str) / sizeof(str[0]);
+    char *ptr2;
+    ptr2 = &str[0];
+    int vowels = countVowels(ptr2, size_str);
+    printf("the number of vowels in the 'str[]' variable is/are: %d\n", vowels);
+
+    /* outro */
+    printf("looking for a royale w/ cheese\n\n");
 
 
     return 0;
