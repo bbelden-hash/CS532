06037f742c2adb7458bba18688f198f8fcd1cb93
Ben Belden <bbelden@uab.edu>
2026-06-24 17:08:27 +0000

    ls.c

---
+++ shellStuff/lsCommand.c
@@ -0,0 +1,27 @@
+
+#include <dirent.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+
+    DIR *dp;
+    struct dirent *dirp;
+
+    if (argc != 2) {
+        fprintf(stderr, "usage: %s directory_name\n", argv[0]);
+        exit(1);
+    }
+
+    if ((dp = opendir(argv[1])) == NULL) {
+        perror("opendir");
+        exit(1);
+    }
+
+    while ((dirp = readdir(dp)) != NULL) {
+        printf("%s\n", dirp->d_name);
+    }
+
+    closedir(dp);
+    return(0);
+}
