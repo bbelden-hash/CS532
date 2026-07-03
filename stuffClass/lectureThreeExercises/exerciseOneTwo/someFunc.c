5137d99eeb0e8276c465f41379e259b4b4a16006
Ben Belden <bbelden@uab.edu>
2026-07-03 19:40:04 +0000

    this took much too long

---
+++ stuffClass/lectureThreeExercises/exerciseOneTwo/someFunc.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "someFunc.h"
+
+void printToTerminal() {
+
+    printf("It is now the fall of my second year in Paris.\nI was sent here for a reason I have not yet been able to fathom.\nI have no money, no resources, no hopes. I am the happiest man alive.\n");
+        
+}
