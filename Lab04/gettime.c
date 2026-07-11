cc51d38d1b79c02c31c0ec65cf780e93ce975899
Ben Belden <bbelden@uab.edu>
2026-07-11 16:48:03 -0500

    Delete Lab04 directory

--- Lab04/gettime.c
+++
@@ -1,9 +0,0 @@
-/* includes the header so this file knows about the function declaration and required libraries */
-#include "gettime.h"
-
-double gettime(void) {
-
-    struct timeval tval;
-    gettimeofday(&tval, NULL);
-    return ((double) tval.tv_sec + (double) tval.tv_usec / 1000000.0);
-}
