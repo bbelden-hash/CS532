32f66a0fd7f76c8deda85508ccdd3520e9f87500
Ben Belden <bbelden@uab.edu>
2026-07-21 17:17:50 +0000

    finishing up _lecture05

--- Labs/Lab07/_lecture05/p1.c
+++ Labs/Lab07/_lecture05/p1.c
@@ -4,9 +4,19 @@
 int main(int argc, char **argv) {
 
     printf("Hello from p1, process id= () %d\n", getpid());
-    char *args[] = {"Hello", "CS", "332", NULL};
-    execv("./obj/p2", args);
-    printf("we are not supposed to see this text");
-    return 0;
 
+    int pid = fork();
+    int status;
+
+    if (pid == 0) {
+
+        char *args[] = {"Hello", "CS", "532", NULL};
+        execv("./obj/p2", args);
+        printf("we are not supposed to see this text");
+    
+    } else if (pid > 0) {
+        wait(&status);
+    }
+
+    return 0;
 }
